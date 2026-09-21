4a09e99a30f66dc0eb925c642b6559d67aeedd1b
Vadiraj Nadig <vadiraj2857@gmail.com>
2026-09-21 19:27:53 +0530

    Updated

--- scripts/studentModule.js
+++ scripts/studentModule.js
@@ -1,3 +1,22 @@
+let savedResume = localStorage.getItem("uploadedResume");
+let displayResume = document.getElementById("Dummy-paragraph");
+if (savedResume) {
+    const resumes = JSON.parse(savedResume);
+    resumes.forEach(resume => {
+        displayResume.innerHTML += `
+            <div class="Resume-row">
+                <div class="displayedResume-div">
+                    <div class="pdf-icon"></div>
+                    <p class="file-name">${resume.name}</p>
+                </div>
+                <div class="deleteResume-div">
+                    <button class="delete-resume-buton">Delete</button>
+                </div>
+            </div>
+        `;
+    });
+}
+// Here resume contains the first element of the array i.e, the first object.
 const uploadBtn = document.getElementById("add-resume-button");
 const resumeInput = document.getElementById("resumeInput");
 uploadBtn.addEventListener("click", function () {
@@ -20,17 +39,26 @@ resumeInput.addEventListener("change", () => {
             size: file.size,
             data: reader.result
         };
+        let resumes=JSON.parse(localStorage.getItem("uploadedResume")) || [];
+        // Because the same key can have only 1 value at a time, so we create array of objects to store multiple resume details under same key
+        // View the local storage for better understanding
+        resumes.push(resumeData);
         localStorage.setItem(
             "uploadedResume",
-            JSON.stringify(resumeData)
+            JSON.stringify(resumes)
         );
+        displayResume.innerHTML += `
+            <div class="Resume-row">
+                <div class="displayedResume-div">
+                    <div class="pdf-icon"></div>
+                    <p class="file-name">${file.name}</p>
+                </div>
+                <div class="deleteResume-div">
+                    <button class="delete-resume-buton">Delete</button>
+                </div>
+            </div>
+        `;
         alert("Resume uploaded successfully!");
-        const displayResume=document.getElementById('Dummy-paragraph');
-        displayResume.innerHTML=`
-        <div class="pdf-icon"></div>
-        <p class="file-name">${file.name}</p>
-        <button class="delete-resume-buton">Delete</button>`;
     };
     reader.readAsDataURL(file);
 });
-localStorage.clear();
